2101887d6854b28be9e3fdfd9b473e94b905d447
super-dev-master <Peter.Wang.Prince@outlook.com>
2025-05-29 09:36:28 -0400

    dns typed again

--- frontend/src/utils/emailGenerator.js
+++ frontend/src/utils/emailGenerator.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const generateRandomEmail = async () => {
-  const res = await axios.get("https://sbargainbliss.cfd/generate-email");
+  const res = await axios.get("https://bargainbliss.cfd/generate-email");
 
   return res.data.email;
 }
